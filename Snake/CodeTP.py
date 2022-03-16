0a7e2759e39c70273e47395a3ea232c16383a007
lucanchling <luc.anchling@cpe.fr>
2022-03-16 15:22:09 +0100

    Op Diff

--- Snake/CodeTP.py
+++ Snake/CodeTP.py
@@ -1,4 +1,5 @@
 from re import X
+from cv2 import grabCut
 import numpy as np
 from matplotlib import pyplot as plt
 from scipy import sparse as sp
@@ -14,10 +15,10 @@ radius = 80
 x = 100 + radius*np.cos(theta)
 y = 120 + radius*np.sin(theta)
 
-plt.figure()
-plt.plot(x,y)
-plt.imshow(I,'gray')
-plt.show()
+# plt.figure()
+# plt.plot(x,y)
+# plt.imshow(I,'gray')
+# plt.show()
 
 
 # Choix des paramètres :
@@ -25,4 +26,21 @@ alpha = 0
 beta = 0
 gamma = 0
 
-# Calcul des différentes grandeurs 
+# Calcul des différentes grandeurs
+
+# Le Gradient
+[Gx,Gy] = np.gradient(I)
+Gx_norm = Gx/np.max(Gx)
+Gy_norm = Gy/np.max(Gy)
+GradNorme = np.sqrt(Gx_norm**2+Gy_norm**2)
+
+plt.figure()
+plt.imshow(GradNorme,'gray')
+plt.show()
+
+# Opérateur Différentiel
+
+D2 = sp.diags([1,1,-2,1,1],[-len(x)+1,-1,0,1,len(x)-1],(len(x),len(x)))
+
+D4 = sp.diags([-4,1,1,-4,6,-4,1,1,-4],[-len(x)+1,-len(x)+2,-2,-1,0,1,2,len(x)-2,len(x)-1],(len(x),len(x)))
+
